68f34490bea7690ab806435da0045be9e7218e98
mraabhijit <abhijityachak@gmail.com>
2026-02-19 12:56:26 +0530

    add rate-limiter; hide backend behind rate-limiter

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api';
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:80/api';
 
 console.log('API Base URL:', baseURL);
 
